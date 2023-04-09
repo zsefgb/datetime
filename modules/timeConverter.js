85466de481195a7e1d5788142752a96fb90433a4
mynameishomin <mynameishomin@gmail.com>
2023-04-09 22:36:52 +0900

    모듈 활성화를 위해 package.json 생성, timeConverter 메소드 추가

---
+++ modules/timeConverter.js
@@ -0,0 +1,35 @@
+export default function timeConverter(from, to, time) {
+    var ms;
+    switch (from) {
+        case "ms":
+            ms = time;
+            break;
+        case "s":
+            ms = time * 1000;
+            break;
+        case "m":
+            ms = time * 60 * 1000;
+            break;
+        case "h":
+            ms = time * 60 * 60 * 1000;
+            break;
+        case "d":
+            ms = time * 24 * 60 * 60 * 1000;
+            break;
+    }
+
+    switch (to) {
+        case "ms":
+            return ms;
+        case "s":
+            return ms / 1000;
+        case "m":
+            return ms / 60 / 1000;
+        case "h":
+            return ms / 60 / 60 / 1000;
+        case "d":
+            return ms / 24 / 60 / 60 / 1000;
+    }
+}
+
+console.log(timeConverter("h", "s", 1));
